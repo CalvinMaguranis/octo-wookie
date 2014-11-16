9d93b1979269f6b92dc8f18848a5f1d9de05e646
Calvin Maguranis <c.maguranis@gmail.com>
2014-11-15 19:02:18 -0500

    added new cross_platform.h/cpp files for (mostly) UTF-8 endcoding functions...more to come probably, will likely need to split this up later

---
+++ cross_platform.cpp
@@ -0,0 +1,5 @@
+#include "cross_platform.h"
+
+void dummy(void) {
+	const char* dumdum = "I'm a dummy function! Yay!";
+}
---
+++ cross_platform.h
@@ -0,0 +1,6 @@
+#ifndef __CROSS_PLATFORM_TOOLS_H
+#define __CROSS_PLATFORM_TOOLS_H
+
+void dummy(void);
+
+#endif // __CROSS_PLATFORM_TOOLS_H
